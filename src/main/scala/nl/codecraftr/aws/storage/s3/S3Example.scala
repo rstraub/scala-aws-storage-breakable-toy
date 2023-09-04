82ffbb2684d3bc903e238ce419b014032a0ee572
Roy <rstraub@xebia.com>
2023-09-04 16:29:10 +0200

    Delete and save cat as a json file

--- src/main/scala/nl/codecraftr/aws/storage/s3/S3Example.scala
+++ src/main/scala/nl/codecraftr/aws/storage/s3/S3Example.scala
@@ -1,13 +1,23 @@
 package nl.codecraftr.aws.storage.s3
 
-import software.amazon.awssdk.auth.credentials.{AwsBasicCredentials, StaticCredentialsProvider}
+import software.amazon.awssdk.auth.credentials.{
+  AwsBasicCredentials,
+  StaticCredentialsProvider
+}
+import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.S3Client
-import software.amazon.awssdk.services.s3.model.Bucket
+import software.amazon.awssdk.services.s3.model.{
+  Bucket,
+  DeleteObjectRequest,
+  PutObjectRequest
+}
 
 import java.util
 
 object S3Example {
+  private val bucketKey = "cats-1.json"
+
   def main(args: Array[String]): Unit = {
     val accessKey = args(0)
     val secretKey = args(1)
@@ -23,6 +33,44 @@ object S3Example {
       .credentialsProvider(StaticCredentialsProvider.create(credentials))
       .build()
 
+    listS3Buckets(s3client)
+
+    deleteCat(s3client)
+    saveCat(s3client)
+
+    s3client.close()
+  }
+
+  private def deleteCat(client: S3Client) = {
+    client.deleteObject(
+      DeleteObjectRequest.builder
+        .bucket("aws-scala-bucket")
+        .key(bucketKey)
+        .build()
+    )
+  }
+
+  private def saveCat(s3client: S3Client) = {
+    val request =
+      PutObjectRequest.builder
+        .bucket("aws-scala-bucket")
+        .key(bucketKey)
+        .contentType("application/json")
+        .build
+
+    val json =
+      """
+          |{
+          |    "name": "Garfield",
+          |    "age": 38,
+          |    "colour": "ginger and black"
+          |}
+          |""".stripMargin
+
+    s3client.putObject(request, RequestBody.fromString(json))
+  }
+
+  private def listS3Buckets(s3client: S3Client): Unit = {
     val listBucketsResponse = s3client.listBuckets
 
     // Display the bucket names
---
+++ src/test/scala/nl/codecraftr/aws/storage/model/CatTest.scala
@@ -0,0 +1,12 @@
+package nl.codecraftr.aws.storage.model
+
+import nl.codecraftr.aws.storage.model.Cat.garfield
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.funsuite.AnyFunSuiteLike
+import org.scalatest.matchers.should.Matchers
+
+class CatTest extends AnyFlatSpec with Matchers {
+  "serialize" should "return json" in {
+//    garfield.toJson
+  }
+}
